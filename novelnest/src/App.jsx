327cf57247830174e60e32ee66e1d7bca7c36619
NduefunaKingsley <162625969+NduefunaKingsley@users.noreply.github.com>
2024-05-13 12:14:43 -0700

    adding new files to main

---
+++ novelnest/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css'
+import AppRouter from '../router/AppRouter.jsx'
+
+function App() {
+  return (
+    <div>
+        <AppRouter />
+    </div>
+  )
+}
+
+export default App
---
+++ novelnest/src/main/home/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div>
+        This is my Home Page
+    </div>
+  )
+}
+
+export default Home
