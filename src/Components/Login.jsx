22ddf91e212203b658f7059b5fb4ca0b758cc7c2
Bismark668 <147917491+Bismark668@users.noreply.github.com>
2024-07-24 12:38:52 +0000

    fully responsive

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -33,53 +33,51 @@ export default class Login extends Component {
   
   render() {
     return (
-      <div className='login-div'>
-        <div className="img-div">
-          <div className="des-div">
-            <h1>Explore,Buy and read all kinds of books</h1> 
+      <div className='w-full md:h-[80%] h-[100vh] md:w-[80%]  grid grid-cols-1 md:grid-cols-2 gap-4 bg-green-300'>
+        <div className="img-div w-full h-full flex flex-col items-center justify-center relative overflow-hidden px-5">
+          <div className="overflow-hidden absolute bottom-20">
+            <h1 className='text-2xl font-extrabold text-green-300 '>Explore,Buy and read all kinds of books</h1> 
           </div>
 
-          <div className="notify-div">
-           <p>Don't have an acount ?</p>
+          <div className="notify-div flex absolute items-center w-[90%] bottom-3  border border-white rounded-xl justify-between overflow-hidden p-2">
+           <p className='text-blue-800 font-extrabold text-xl'>Don't have an acount ?</p>
            <Link to={'/register'}>
-              <button>Sign-Up</button>
+              <button className='text-white bg-green-500 border-none outline-none cursor-pointer p-2 text-xl rounded-md'>Sign-Up</button>
            </Link>
-            
           </div>
-
         </div>
-        <div className="form-div">
-          <div className="lgo">
-            <img className='logo-img' src={Logo} alt="logo" />
-            <h4>Welcome !</h4>
+        <div className="form-div w-full h-full items-center justify-start flex flex-col ">
+          <div className="w-full flex items-center flex-col gap-2">
+            <img className='w-[150px] h-[150px] ' src={Logo} alt="logo" />
+            <h4 className='text-xl '>Welcome !</h4>
           </div>
 
           <form onSubmit={this.handleSubmit}>
-            <div className="forms">
-              <div className="input-div">
-                <label htmlFor="Username">Username</label>
-                <div className="input">
+            <div className="forms flex flex-col items-center justify-center gap-4">
+              <div className="flex flex-col items-start gap-1">
+                <label className='text-black block py-1 text-xl' htmlFor="Username">Username</label>
+                <div className="flex gap-2 p-2 bg-white">
                   <FaUser/>
-                  <input type="text" id='Username' value={this.state.UserName} onChange={this.handleUserNameailChange} placeholder='Enter Username'/>
+                  <input className='bg-none outline-none border-none min-w-[250px]' type="text" id='Username' value={this.state.UserName} onChange={this.handleUserNameailChange} placeholder='Enter Username'/>
                 </div>
               </div>
 
-              <div className="input-div">
-                <label htmlFor="Password">Password</label>
-                <div className="input">
+              <div className="flex flex-col items-start gap-1">
+                <label className='text-black block py-1 text-xl' htmlFor="Password">Password</label>
+                <div className="flex gap-2 p-2 bg-white">
                  <FaUnlockKeyhole />
-                  <input type="password" id='password' value={this.state.Password} onChange={this.handlePasswordChange} placeholder='Enter password'/>
+                  <input className='bg-none outline-none border-none min-w-[250px]' type="password" id='password' value={this.state.Password} onChange={this.handlePasswordChange} placeholder='Enter password'/>
                 </div>
               </div>
 
-              <Link to={'/dashboard'}>
-                <button className='btn'>
+              <Link className='w-[70%]' to={'/dashboard'}>
+                <button className=' self-center text-white flex items-center justify-evenly w-[80%] m-auto bg-green-500 outline-none rounded-lg cursor-pointer text-xl px-3 py-2'>
                   Sign-in
                   <FaRegArrowAltCircleRight />
                 </button>
               </Link>
 
-              <p>Forgot your password ? <Link to={'/forgotPassword'}>Click here</Link></p>
+              <p>Forgot your password ? <Link className='text-blue-600 text-xl' to={'/forgotPassword'}>Click here</Link></p>
             </div>
           </form>
         </div>
--- src/Components/Register.jsx
+++ src/Components/Register.jsx
@@ -49,58 +49,58 @@ export default class Register extends Component {
 
   render() {
     return (
-    <div className="login-div">
-        <div className='img-div'>
-            <div className="des-div">
-                <h1>Explore,Buy and read all kinds of books</h1>
+    <div className="w-full md:h-[80%] h-[100vh] md:w-[80%]  grid grid-cols-1 md:grid-cols-2 gap-4 bg-green-300">
+        <div className='img-div img-div w-full h-full flex flex-col items-center justify-center relative overflow-hidden px-5'>
+            <div className="overflow-hidden absolute bottom-20">
+                <h1 className='text-2xl font-extrabold text-green-300 '>Explore,Buy and read all kinds of books</h1>
              </div>
-            <div className='notify-div'>
-                <p>Already have an acount ?</p>
-                <Link to={'/login'}><button>Sign-in</button></Link>
+            <div className='notify-div flex absolute items-center w-[90%] bottom-3  border border-white rounded-xl justify-between overflow-hidden p-2'>
+                <p className='text-blue-800 font-extrabold text-xl' >Already have an acount ?</p>
+                <Link to={'/login'}><button className='text-white bg-green-500 border-none outline-none cursor-pointer p-2 text-xl rounded-md' >Sign-in</button></Link>
              </div>
         </div>
 
-        <div className="form-div">
-            <div className='logo'>
-                <img className='logo-img' src={Logo} alt="logo" />
-                <h4>Create New Account !</h4>
+        <div className="form-div w-full h-full items-center justify-start flex flex-col  ">
+            <div className='w-full flex items-center flex-col'>
+                <img className='w-[100px] h-[100px]' src={Logo} alt="logo" />
+                <h4  className='text-xl '>Create New Account !</h4>
             </div>
 
             <form onSubmit={this.handleSubmit}>
-                <div className="forms">
-                    <div className="input-div">
-                     <label htmlFor="email">Email</label>
-                        <div className="input">
+                <div className="forms flex flex-col items-center justify-center md:gap-2 gap-4">
+                    <div className="flex flex-col items-start">
+                     <label className='text-black block  text-xl'  htmlFor="email">Email</label>
+                        <div className="flex gap-2 p-2 bg-white">
                             <MdEmail />
-                            <input type="email" id='email' value={this.state.Email} onChange={this.handleEmailChange} placeholder='Enter Email'/>
+                            <input  className='bg-none outline-none border-none min-w-[250px]' type="email" id='email' value={this.state.Email} onChange={this.handleEmailChange} placeholder='Enter Email'/>
                         </div>
                     </div>
 
-                    <div className="input-div">
-                        <label htmlFor="username">Username</label>
-                        <div className="input">
+                    <div className="flex flex-col items-start">
+                        <label className='text-black block  text-xl' htmlFor="username">Username</label>
+                        <div className="flex gap-2 p-2 bg-white">
                             <FaUser/>
-                            <input type="text" id='username' value={this.state.UserName} onChange={this.handleUserNameailChange} placeholder='Enter Username'/>
+                            <input  className='bg-none outline-none border-none min-w-[250px]' type="text" id='username' value={this.state.UserName} onChange={this.handleUserNameailChange} placeholder='Enter Username'/>
                         </div>
                     </div>
 
-                    <div className="input-div">
-                        <label htmlFor="password">Create Password</label>
-                         <div className="input">
+                    <div className="flex flex-col items-start">
+                        <label className='text-black block  text-xl' htmlFor="password">Create Password</label>
+                         <div className="flex gap-2 p-2 bg-white">
                             <FaUserShield />
-                            <input type="password" id='password' value={this.state.Password} onChange={this.handlePasswordChange} placeholder='Enter Password'/>
+                            <input  className='bg-none outline-none border-none min-w-[250px]' type="password" id='password' value={this.state.Password} onChange={this.handlePasswordChange} placeholder='Enter Password'/>
                         </div>
                     </div>
 
-                    <div className="input-div">
-                         <label htmlFor="password">Confirm Password</label>
-                        <div className="input">
+                    <div className="flex flex-col items-start">
+                         <label className='text-black block text-xl' htmlFor="password">Confirm Password</label>
+                        <div className="flex gap-2 p-2 bg-white">
                              <FaUserShield />
-                            <input type="password" id='password' placeholder='Confirm Password'/>
+                            <input  className='bg-none outline-none border-none min-w-[250px]' type="password" id='password' placeholder='Confirm Password'/>
                         </div>
                     </div>
-                    <Link to={'/login'}>
-                        <button type='submit' className='btn'>
+                    <Link className='w-[70%]' to={'/login'}>
+                        <button  className=' self-center text-white flex items-center justify-evenly w-[80%] m-auto bg-green-500 outline-none rounded-lg cursor-pointer text-xl px-3 py-2'  type='submit' className='btn'>
                             Sign-up
                             <FaRegArrowAltCircleRight />
                         </button>
